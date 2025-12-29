8bada4df7a6368802281e050c4785d93de78fd71
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-29 16:45:31 +0530

    create our login logic

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,26 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    
+    CredentialsProvider({
+      // 'Sign in with {name} button
+      name: "Credentials",
+
+      // form input
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        //   my own login logic
+        return null;
+      },
+    }),
+
     // ...add more providers here
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
