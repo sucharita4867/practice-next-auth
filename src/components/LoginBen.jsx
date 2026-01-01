05dcaf3d465d5023e0ce61e45d0e026d21ac8c44
sucharita4867 <sucharitasardar7@gmail.com>
2026-01-01 23:31:23 +0530

    delete login new btn

--- src/components/LoginBen.jsx
+++ src/components/LoginBen.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 const LoginBen = () => {
   return <button className="btn">Login Now</button>;
-  return <button className="btn">Login Now</button>;
 };
 
 export default LoginBen;
